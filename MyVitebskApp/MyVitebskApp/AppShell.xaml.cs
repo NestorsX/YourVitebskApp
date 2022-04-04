bae2f76510221ed1fcee825191ead3be6ef2e1e1
Кирилл Нестерович <nesterkalol@gmail.com>
2022-04-04 15:06:08 +0300

    Add project files.

---
+++ MyVitebskApp/MyVitebskApp/AppShell.xaml.cs
@@ -0,0 +1,20 @@
+﻿using MyVitebskApp.ViewModels;
+using MyVitebskApp.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace MyVitebskApp
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute("//LoginPage", typeof(LoginPage));
+            //Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            //Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+    }
+}
